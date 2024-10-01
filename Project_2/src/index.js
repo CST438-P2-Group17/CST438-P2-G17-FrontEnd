9e682181cba154bee33fa87f4fe7cb59881ff0ec
Sergio Larios <106183306+slariosjr@users.noreply.github.com>
2024-10-01 01:13:42 -0700

    Revert "Login page"

--- Project_2/src/index.js
+++ Project_2/src/index.js
@@ -3,18 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import { BrowserRouter as Router } from 'react-router-dom';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId="591552019563-hc7e24ispsfj464umknr2u428obj0fsh.apps.googleusercontent.com">
-      <Router>
-        <App />
-      </Router>
-    </GoogleOAuthProvider>
+    <App />
   </React.StrictMode>
 );
 
